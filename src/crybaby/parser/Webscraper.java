119e3c8a16fed62d9fc86a27bbafcf3280cbd7c3
Joshua Cranmer <Pidgeot18@gmail.com>
2010-11-04 19:42:24 -0400

    Updating milestone

--- src/crybaby/parser/Webscraper.java
+++ src/crybaby/parser/Webscraper.java
@@ -12,6 +12,22 @@ public class Webscraper {
 		page = new HtmlDocumentBuilder().parse(uri);
 	}
 	
+	public void filterPage() {
+		// The following is a list of tags to remove
+		removeList(page.getElementsByTagName("head"));
+		removeList(page.getElementsByTagName("script"));
+		removeList(page.getElementsByTagName("style"));
+		removeList(page.getElementsByTagName("object"));
+	}
+	
+	private void removeList(NodeList list) {
+		System.out.println(list.getLength());
+		for (int i = 0; i < list.getLength(); i++) {
+			Node n = list.item(i);
+			n.getParentNode().removeChild(n);
+		}
+	}
+	
 	public String dumpText() {
 		Element body = (Element)page.getElementsByTagName("body").item(0);
 		return body.getTextContent();
@@ -40,6 +56,10 @@ public class Webscraper {
 
 	public static void main(String[] args) throws Exception{
 		Webscraper scraper = new Webscraper(args[0]);
+		String before = scraper.dumpText();
+		scraper.filterPage();
+		String after = scraper.dumpText();
+		System.out.println(before.equals(after));
 		System.out.println(scraper.dumpText());
 	}
 

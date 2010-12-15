05bfe5602f126c2451737b9e6ecb4a5267c349f6
matt <matt@localhost>
2010-12-15 15:59:03 -0500

    Added a couple more scripts, for individual components

---
+++ jargon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Runs the jargon extractor test
+
+java -Xmx1500M -cp ./stanford-parser.jar;./src \
+    crybaby.jargon.JargonExtractor $1
---
+++ summarize.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Runs the summarizer test.
+
+java -cp ./src crybaby.summarize.SummarizerTest $1
